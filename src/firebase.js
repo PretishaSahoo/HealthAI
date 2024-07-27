197b4b96e4dbe8d06e714fa3d23775f3caa54ed8
Pretisha Sahoo <pretishasahoo@gmail.com>
2024-07-27 19:24:36 +0530

    fix

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API,
-  authDomain: process.env.REACT_APP_AUTH,
+  authDomain: process.env.REACT_APP_AU,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
