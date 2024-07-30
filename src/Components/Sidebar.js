9ac5882c479f59a6d07ac0ee096c6c4813ceb43b
Pretisha Sahoo <pretishasahoo@gmail.com>
2024-07-30 20:24:04 +0530

    sidebar fix

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -41,14 +41,6 @@ export default function Sidebar({ isSidebarOpen, setIsSidebarOpen }) {
             <Link to="/dashboard/applydoctor" className="text-purple-900">Apply as a Doctor</Link>
           </div>}
         </div>
-        <button
-          onClick={() => setIsSidebarOpen(false)}
-          className="lg:hidden fixed top-4 right-4 z-20 p-2 bg-purple-900 text-white rounded-full shadow-md"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
       </div>
     </>
   );  
