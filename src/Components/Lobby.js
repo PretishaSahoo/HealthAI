ceade841c0426fbdd1bbba189d52d53a83f62432
Pretisha Sahoo <pretishasahoo@gmail.com>
2024-10-17 20:24:37 +0530

    ...

--- src/Components/Lobby.js
+++ src/Components/Lobby.js
@@ -50,7 +50,7 @@ export default function Lobby() {
             <form className="flex flex-col items-center w-[90%] sm:w-[40%] mx-auto p-6 m-6 mt-32 border border-violet-300 rounded-xl">
                 <label htmlFor="room" className="sr-only">Room Code</label>
                 <input value={room} type="text" className="rounded-xl mb-2 p-4 w-full bg-violet-100 border border-violet-300 text-center" placeholder="Enter room code" readOnly />
-                <button onClick={handleSubmit} className="p-4 mt-2 text-center rounded-lg bg-gradient-to-tr from-pink-300 via-purple-400 to-violet-700 m-2 text-sm md:text-base font-semibold transition hover:from-violet-700 hover:to-pink-300 shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 w-full text-white rounded-xl">Join</button>
+                <button onClick={handleSubmit} className="p-4 mt-2 text-center bg-gradient-to-tr from-pink-300 via-purple-400 to-violet-700 m-2 text-sm md:text-base font-semibold transition hover:from-violet-700 hover:to-pink-300 shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 w-full text-white rounded-xl">Join</button>
             </form>
         </div>
     );
